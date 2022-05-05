6a9f2642e95c4b0a083d0a1dd3ad6ec55da15adf
docziadrian <docziadrianmark@turr.hu>
2022-05-05 13:17:20 +0200

    Új grafikon beszúrása

--- grafikon.js
+++ grafikon.js
@@ -1,6 +1,8 @@
 function grafikon1() {
   var graf2 = document.getElementById("graf2");
   graf2.style.display = "none";
+  var graf3 = document.getElementById("graf3");
+  graf3.style.display = "none";
   var x = document.getElementById("graf1");
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -12,7 +14,22 @@ function grafikon1() {
 function grafikon2() {
   var graf1 = document.getElementById("graf1");
   graf1.style.display = "none";
+  var graf3 = document.getElementById("graf3");
+  graf3.style.display = "none";
   var x = document.getElementById("graf2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function grafikon3() {
+  var graf2 = document.getElementById("graf2");
+  graf2.style.display = "none";
+  var graf1 = document.getElementById("graf1");
+  graf1.style.display = "none";
+  var x = document.getElementById("graf3");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
